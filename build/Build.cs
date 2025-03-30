031a87218432313083ec5e90a64e3a31dcb171c7
Christian Flessa <chris@z0rn.net>
2025-03-30 12:12:32 +0200

    Add packing and publishing configurations for NuGet release

--- build/Build.cs
+++ build/Build.cs
@@ -7,22 +7,39 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
 using System;
 using System.Collections.Generic;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 internal class Build : NukeBuild,
                        IHazArtifacts,
                        IHazSolution,
                        IHazGitRepository,
                        ICompile,
-                       ITest
+                       ITest,
+                       IPack
 {
     public Configure<DotNetBuildSettings> CompileSettings => settings =>
         settings.EnableContinuousIntegrationBuild()
                 .EnableTreatWarningsAsErrors();
 
     public Configuration Configuration => Configuration.Release;
+
+    public Configure<DotNetPublishSettings> PublishSettings => settings =>
+        settings.EnableContinuousIntegrationBuild();
+
+    public Configure<DotNetPackSettings> PackSettings => settings =>
+        settings.DisablePackageRequireLicenseAcceptance()
+                .EnableContinuousIntegrationBuild()
+                .ResetNoBuild()
+                .SetProject("src/NtfyCator/NtfyCator.csproj")
+                .AddProperty("AdditionalConstants", "NUGET_RELEASE")
+                .AddProperty("SignAssembly", "true")
+                .AddProperty("AssemblyOriginatorKeyFile", "../../NtfyCator.snk")
+                .EnableIncludeSymbols()
+                .SetSymbolPackageFormat(DotNetSymbolPackageFormat.snupkg);
 
     private static AbsolutePath TestsDirectory => RootDirectory / "tests";
 
@@ -37,6 +54,18 @@ internal class Build : NukeBuild,
               });
 
     private AbsolutePath SourceDirectory => RootDirectory / "src";
+
+    Target IPack.Pack => target =>
+        target.Produces(From<IPack>().PackagesDirectory / "*.nupkg")
+              .Executes(() =>
+              {
+                  DotNetPack(_ => _
+                                  .Apply(From<IPack>().PackSettingsBase)
+                                  .Apply(PackSettings));
+
+                  ReportSummary(_ => _
+                                    .AddPair("Packages", From<IPack>().PackagesDirectory.GlobFiles("*.nupkg").Count.ToString()));
+              });
 
     IEnumerable<Project> ITest.TestProjects => Partition.GetCurrent(From<IHazSolution>().Solution.GetAllProjects("*.Tests"));
 
